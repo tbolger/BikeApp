856259427a7ba89cdd363416041e0996a16743c3
“johngrhm6git config --global user.email “johngrhm6@gmail.com <YOUR@EMAIL.com>
2016-03-29 04:29:12 +0100

    Added image processor,edited new bike form and styled

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -4,11 +4,32 @@ class BikesController < ApplicationController
   end
 
   def index
+  	@bikes =Bike.all
   end
 
   def show
+  	@bike = Bike.find(params[:id])
   end
 
   def edit
+    @bike = Bike.find(params[:id])
+  end
+  def create
+  	@bike = Bike.new(permit_bike)
+  	if @bike.save
+  		flash[:success] ="Bike has been created successfully"
+  		redirect_to bike_path(@bike)
+  	else
+  		flash[:error] = @bike.errors.full_messages
+  		redirect_to new_bike_path
+
+  	end
+  	
+  end
+
+  private 
+  def permit_bike
+  	params.require(:bike).permit(:image,:description,:price,:manufacturer)
+  	
   end
 end
